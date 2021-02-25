d2e84ad9fbf285a21a4dd946cfb42873adc46c23
mihai <mihai.andrei1597@gmail.com>
2021-02-25 16:51:24 +0200

    mongodb connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,24 +1,12 @@
-// const express = require('express');
-// const products = require('./data/products');
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.send('API is running...');
-// });
-
-// app.get('/api/products', (req, res) => {
-//   res.json(products);
-// });
-
-// app.get('/api/products/:id', (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id);
-//   res.json(product);
-// });
-
-// app.listen(5000, () => console.log('Server running on port 5000'));
-
 import products from './data/products';
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import 'colorts/lib/string';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -35,4 +23,11 @@ app.get('/api/products/:id', (req: Request, res: Response) => {
   res.json(product);
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
+  ),
+);
